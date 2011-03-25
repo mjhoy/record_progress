8b7c943ef754dbb8bdcb539250d042f228c6e47e
Michael Hoy <michael.john.hoy@gmail.com>
2011-03-24 23:15:34 -0400

    add Protovis JavaScript

---
+++ user-graph.js
@@ -0,0 +1,15 @@
+(function () {
+
+  if (Drupal.jsEnabled) {
+
+    $(document).ready(function () {
+
+      $('dd.tracking-data-graph-container').text('Loading protovis graph...');
+
+      // todo... load the graph
+
+    });
+
+  }
+
+}());
