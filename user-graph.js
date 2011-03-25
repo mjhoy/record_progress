39fefd0d7a55add4dc69ce0c79955003e4183c78
Michael Hoy <michael.john.hoy@gmail.com>
2011-03-25 09:08:56 -0400

    initial graph render

--- user-graph.js
+++ user-graph.js
@@ -8,25 +8,76 @@
 
   if (Drupal.jsEnabled) {
 
-    /**
-     * Set up the protovis graph in `domElement` for `dataSet`.
-     */
-    function setupProtovisGraph (domElement, name, dataSet) {
-      var min_x, min_y, max_x, max_y;
-
-      // Uses filter `filter` (meant to be either _.min or _.max) 
-      // on property `property` for an array of arrays.
-      function nestedFilter(filter, property) {
-        return filter(_.map(dataSet, function(data) {
-          return parseInt(filter(data, function (i) { 
-            return parseInt(i[property], 10);} )[property], 10);
+    // Uses filter `filter` (meant to be either _.min or _.max) 
+    // on property `property` for an array of arrays.
+    function nestedFilter(dataSet, filter, property) {
+      return filter(_.map(dataSet, function(data) {
+        return parseInt(filter(data, function (i) { 
+          return parseInt(i[property], 10);} )[property], 10);
         }));
       }
 
-      min_x = nestedFilter(_.min, 'x');
-      min_y = nestedFilter(_.min, 'y');
-      max_x = nestedFilter(_.max, 'x');
-      max_y = nestedFilter(_.max, 'y');
+    /**
+     * Set up the protovis graph in `domElement` for `dataSet`.
+     */
+    function setupProtovisGraph (domEl, name, dataSet) {
+      var vis,
+          min_x = nestedFilter(dataSet, _.min, 'x'),
+          min_y = nestedFilter(dataSet, _.min, 'y'),
+          max_x = nestedFilter(dataSet, _.max, 'x'),
+          max_y = nestedFilter(dataSet, _.max, 'y'),
+          w = 600,
+          h = 200,
+          x = pv.Scale.linear((min_x - 40), (max_x + 40)).range(0, w),
+          y = pv.Scale.linear((min_y - 50), (max_y + 50)).range(0, h);
+
+      vis = new pv.Panel()
+        .width(w)
+        .height(h)
+        .canvas(domEl);
+
+      /* Y-axis rule */
+      vis.add(pv.Rule)
+        .data(pv.range(0, (max_y + 50), 50))
+        .bottom(function(d) { if (y(d) > 10) {return  y(d);} })
+        .strokeStyle("#aaa")
+        .add(pv.Label);
+
+      var months = [
+        { n: 1, month: "Jan"},
+        { n: 32, month: "Feb"},
+        { n: 60, month: "Mar"},
+        { n: 91, month: "Apr"},
+        { n: 121, month: "May"},
+        { n: 152, month: "Jun"},
+        { n: 182, month: "Jul"},
+        { n: 213, month: "Aug"},
+        { n: 244, month: "Sep"},
+        { n: 274, month: "Oct"},
+        { n: 305, month: "Nov"},
+        { n: 335, month: "Dec"}
+      ];
+
+      /* X-axis rule */
+      vis.add(pv.Rule)
+        .data(months)
+        .left(function(d) { if (x(d['n']) > 15) { return x(d['n']); } else { return -20; } })
+        .strokeStyle("#ccc")
+        .add(pv.Label)
+        .bottom(5)
+        .text(function(d) { return d['month']; });
+
+      _.map(dataSet, function(data, index) {
+        console.log(data);
+        vis.add(pv.Dot)
+          .data(data)
+          .bottom(function(d) { return y(d['y']); })
+          .left(function(d) { return x(d['x']); })
+          .strokeStyle(function (d) { return (dataSet[index]['color'] || "#aaa"); })
+          .add(pv.Line);
+      });
+
+      vis.render();
     }
 
     $(document).ready(function () {
